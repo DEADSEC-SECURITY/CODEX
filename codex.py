41b7872f710ebb1d0e83d8bb3b5d856215aeb6e4
DEADSEC-SECURITY <amng835@gmail.com>
2019-04-03 17:29:29 +0100

    Chunk Stage

--- codex.py
+++ codex.py
@@ -1521,4 +1521,67 @@ class NMAPScan():
 
                 return NMAPScan.SpecificPort()
 
+class Defaults():
+
+    def IP():
+
+        with open('Data/DATA.json') as f:
+            data = json.load(f)
+        newip = input("Please enter your new default ip address: ")
+        data['DefaultInfo'][0]['defaultipaddress'] = defaultipaddr = newip
+        with open('Data/DATA.json', 'w') as f:
+            json.dump(data, f, indent = 2)
+        OS()
+        print("Default ip address changed to " + newip)
+        return StartMenu()
+
+    def PortRange():
+        with open('Data/DATA.json') as f:
+            data = json.load(f)
+
+            newport = input("Please enter your new default port range: ")
+            data['DefaultInfo'][0]['defaultportrange'] = portrange = newport
+
+            with open('Data/DATA.json', 'w') as f:
+                json.dump(data, f, indent = 2)
+
+            OS()
+            print("Default port range changed to " + newport)
+            return Menus.StartMenu()
+
+    def Interface():
+
+        global interface
+        global newinterface
+
+        with open('Data/DATA.json') as f:
+            data = json.load(f)
+
+        for DefaultInfo in data['DefaultInfo']:
+            interface = DefaultInfo['defaultinterface']
+
+        OS()
+        print(f'{B}********** Please select your default interface **********{W}')
+        print(*(W + '{}) {}'.format(x, y) for (x, y) in enumerate(NetworkInterfaces, 1)), sep='\n')
+        print()
+        print(f'{C}----------')
+        print(f'{G}Note: You can change the interface later if you need!')
+        print(f'{C}----------')
+        print(f'{P}Contribution: We are looking for a way to make it so you dont need to write the hole interface name and only a number!')
+        print(f'{C}----------{W}')
+        print()
+        newinterface = input('Please write your new default interface: ')
+
+        data['DefaultInfo'][0]['defaultinterface'] = defaultinterface = newinterface
+        with open('Data/DATA.json', 'w') as f:
+            json.dump(data, f, indent = 2)
+
+        data['DefaultInfo'][0]['defaultinterface'] = defaultinterface = interface
+        with open('Data/DATA.json', 'w') as f:
+            json.dump(data, f, indent = 2)
+
+        print("Interface changed to " + interface)
+
+        return Menus.StartMenu()
+
 Verify.CheckAdminPrivs()
